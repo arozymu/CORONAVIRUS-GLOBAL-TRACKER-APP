663d2e5ede52968ee9a4e3cc9ef748b5db25a61f
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2020-05-04 06:53:30 -0700

    <material-ui>

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props);
+
     return (
         <h1>Cards</h1>
     )
