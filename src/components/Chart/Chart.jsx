13c63bba03c0fe62b90ba820a167d6557f54c3e8
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2020-05-05 10:56:04 -0700

    <barchart>

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = (data, country) => {
     const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
@@ -37,6 +37,15 @@ const Chart = () => {
         }}
         />) : null 
 
+    );
+
+    const barChar = (
+        data.confirmed
+        ? (
+            <Bar
+
+            />
+        ) : null
     );
     return (
         <div className={styles.container}>
